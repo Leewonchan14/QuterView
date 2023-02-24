0fc22407fce675c78927ee24c0861bc8eb5ac009
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-24 15:32:02 +0900

    아이템 스크립트 추가, 컴포넌트 추가

---
+++ Assets/Script/item/item.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour
+{
+    public enum itemType { Ammo, Coin, Grenade, Heart, Weapon };
+    public itemType myType;
+
+}
